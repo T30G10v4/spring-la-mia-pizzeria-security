16943441a7b8634d043a49ec39343382f880c941
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-04-07 15:11:39 +0200

    Security Implemented

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,3 +5,12 @@ INSERT INTO `pizzas` (`id`, `description`, `name`, `price`) VALUES (NULL, 'Pomod
 INSERT INTO `ingredients` (`description`, `name`) VALUES('Carciofini Italiani DOC', 'Carciofini');
 INSERT INTO `ingredients` (`description`, `name`) VALUES('Rovagnati DOC', 'Prosciutto Cotto');
 INSERT INTO `ingredients` (`description`, `name`) VALUES('Olive Nere Siciliane DOC', 'Olive Nere');
+
+INSERT INTO users (email, first_name, last_name, password) VALUES('john@email.it', 'John', 'Doe', '{noop}john');
+INSERT INTO users (email, first_name, last_name, password) VALUES('jane@email.it', 'Jane', 'Smith','{noop}jane');
+
+INSERT INTO roles (id, name) VALUES(1, 'ADMIN');
+INSERT INTO roles (id, name) VALUES(2, 'USER');
+
+INSERT into users_roles(user_id, roles_id) VALUES(1, 1);
+INSERT into users_roles(user_id, roles_id) VALUES(2, 2);
