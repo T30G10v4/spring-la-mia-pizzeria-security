3600ddbe88ae05a5cc301b15bc8adf9bbc1b3ad5
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-04-07 14:23:38 +0200

    First Commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO `pizzas` (`id`, `description`, `name`, `price`) VALUES (NULL, 'Pomodoro, Mozzarella, Tonno, Cipolle', 'Tonno e Cipolle', '8.00')
+INSERT INTO `pizzas` (`id`, `description`, `name`, `price`) VALUES (NULL, 'Pomodoro, Mozzarella, Acciughe, Olive', 'Acciughe e Olive', '8.99')
+INSERT INTO `pizzas` (`id`, `description`, `name`, `price`) VALUES (NULL, 'Pomodoro, Mozzarella, Prosciutto Cotto, Olive, Carciofi, Capperi, Funghi, Acciughe', 'Capricciosa', '8.99')
+
+INSERT INTO `ingredients` (`description`, `name`) VALUES('Carciofini Italiani DOC', 'Carciofini');
+INSERT INTO `ingredients` (`description`, `name`) VALUES('Rovagnati DOC', 'Prosciutto Cotto');
+INSERT INTO `ingredients` (`description`, `name`) VALUES('Olive Nere Siciliane DOC', 'Olive Nere');
